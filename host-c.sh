671c340278bf8c873c3d0f3ab340e190db3935c3
Francesco Malagò <73899142+framspace@users.noreply.github.com>
2021-05-15 21:47:42 +0200

    Update host-c.sh

--- host-c.sh
+++ host-c.sh
@@ -6,7 +6,7 @@ sudo apt -y install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker pull dustnic82/nginx-test
-sudo docker run --name nginx -p 80:80 -d dustnic82/nginx-test
+sudo docker run -p 80:80 -d dustnic82/nginx-test
 #Network setup
 sudo ip addr add 192.168.4.2/24 dev enp0s8
 sudo ip link set dev enp0s8 up
